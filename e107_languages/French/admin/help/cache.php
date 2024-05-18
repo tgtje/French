1c470fa4ad5fe72afde60e6ca9a75c8346a8a100
leo <the_guardian2@hotmail.com>
2024-05-18 13:04:45 +0200

    Update cache.php

--- e107_languages/French/admin/help/cache.php
+++ e107_languages/French/admin/help/cache.php
@@ -15,4 +15,3 @@ if (!defined('e107_INIT')) { exit; }
 $caption = "Syst&egrave;me de cache";
 $text = "Si le cache est activ&eacute;, la fluidit&eacute; du site est sensiblement augment&eacute;e et le nombre de requ&ecirc;tes SQL vers la base de donn&eacute;es r&eacute;duit.<br /><br /><strong>IMPORTANT: Si vous &ecirc;tes en train de modifier le th&egrave;me, d&eacute;sactivez le cache afin que les modifications soient visibles.</strong>";
 $ns -> tablerender($caption, $text);
-Demo Version, Only convert first 5 files at one time, Please buy now!
