87513a7ae26a20fd5d70d50c26b72a5103710e5c
leo <the_guardian2@hotmail.com>
2024-05-18 13:05:04 +0200

    Update cpage.php

--- e107_languages/French/admin/help/cpage.php
+++ e107_languages/French/admin/help/cpage.php
@@ -15,4 +15,3 @@ if (!defined('e107_INIT')) { exit; }
 $text = "&Agrave; partir de cette zone, vous pouvez cr&eacute;er des menus et des pages personnalis&eacute;s avec votre propre contenu.<br />Les menus et les pages sont li&eacute;s de mani&egrave;re &agrave; ce que vous puissiez avoir un menu facilement li&eacute; &agrave; la page si vous le souhaitez.";
 // $text .= "Veuillez consulter <a href='http://docs.e107.org/Using Pages personnalis&eacute;es et menus personnalis&eacute;s&apos;>http://docs.e107.org/Using Pages personnalis&eacute;es et menus personnalis&eacute;s</a> pour une explication de toutes les fonctionnalit&eacute;s." ;
 $ns -> tablerender("Aide pages/menus personnalis&eacute;s", $text);
-Demo Version, Only convert first 5 files at one time, Please buy now!
